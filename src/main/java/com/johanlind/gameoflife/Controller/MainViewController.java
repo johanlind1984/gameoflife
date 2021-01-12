7fa3e4d8aa843e9e1cade8ab5bd68b77867f4be9
johanlind1984 <johanlind1984@gmail.com>
2021-01-12 23:10:08 +0100

    initial commit

---
+++ src/main/java/com/johanlind/gameoflife/Controller/MainViewController.java
@@ -0,0 +1,4 @@
+package com.johanlind.gameoflife.Controller;
+
+public class MainViewController {
+}
---
+++ src/main/java/com/johanlind/gameoflife/Main.java
@@ -0,0 +1,10 @@
+package com.johanlind.gameoflife;
+
+import com.johanlind.gameoflife.View.StartView;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StartView.render();
+    }
+}
