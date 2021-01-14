53f81ff54249b3224b2d8771d696594185d9c793
johanlind1984 <johanlind1984@gmail.com>
2021-01-14 11:57:02 +0100

    added modeltests

---
+++ src/test/java/com/johanlind/gameoflife/modelTests/GameBoardTest.java
@@ -0,0 +1,17 @@
+package com.johanlind.gameoflife.modelTests;
+
+import com.johanlind.gameoflife.Model.Cell;
+import com.jparams.verifier.tostring.ToStringVerifier;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.Test;
+
+public class GameBoardTest {
+
+    @Test
+    public void cellModelTest() {
+
+        ModelsTestUtils.validateAccessors(Cell.class);
+        ToStringVerifier.forClass(Cell.class);
+        EqualsVerifier.forClass(Cell.class);
+    }
+}
