abcc827072d396d8028167d08b498cddf5e4dafd
johanlind1984 <johanlind1984@gmail.com>
2021-01-15 09:23:00 +0100

    removed irrelevant pom changes

--- src/main/java/com/johanlind/gameoflife/Main.java
+++ src/main/java/com/johanlind/gameoflife/Main.java
@@ -6,8 +6,8 @@ import com.johanlind.gameoflife.Controller.GameOfLifeController;
 
 public class Main {
 
-    public static final int height = 50;
-    public static final int width = 50;
+    public static final int height = 100;
+    public static final int width = 100;
 
     public static void main(String[] args) throws InterruptedException {
         GameOfLifeController gameOfLifeController = new GameOfLifeController(height, width);
